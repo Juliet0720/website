72ce973c8d19ba21d23a56e61fe2e4a2411937b6
Juliet0720 <74673422+Juliet0720@users.noreply.github.com>
2025-03-03 21:44:14 +0800

    import images of  projects

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,6 +1,16 @@
 import { NavLink } from "react-router-dom";
 import React from 'react'
-
+import logo from "../assets/Logo only.png"
+import primaryLogo from "../assets/Primary.png"
+import secondaryLogo from "../assets/Secondary.png"
+import pictureOne from "../assets/ID with lanyard.png"
+import pictureTwo from "../assets/letterhead.png"
+import pictureThree from "../assets/Icawalo (1).png"
+import pictureFour from "../assets/Icawalo_Typography (1).png"
+import pictureFive from "../assets/IMG_3024@362102928.jpg"
+import pictureSix from "../assets/IMG_3045.jpg"
+import pictureSeven from "../assets/IMG_3166.jpg"
+import pictureEight from "../assets/6186024448944095280.jpg"
 
 
 const Project = () => {
@@ -23,23 +33,23 @@ const Project = () => {
   <p className="desc">Here's some projects I have done using Adobe Illustrator and Photoshop</p>
  <h3><strong className="strong">Mock up and Logo</strong></h3> 
   <li>
-  <img src="src\assets\Logo only.png" className="mock" alt="Zencreations Logo"></img>
-    <img src="src\assets\Primary.png" className="mock" alt="Logo-Primary" />
-    <img src="src\assets\Secondary.png" className="mock" alt="Logo-Secondary" />
-    <img src="src\assets\ID with lanyard.png" className="mock" alt="ID w/ Lanyard" />
-    <img src="src\assets\Letterhead.png" className="mock" alt="Letterhead" />
+  <img src={logo} className="mock" alt="Zencreations Logo"></img>
+    <img src={primaryLogo} className="mock" alt="Logo-Primary" />
+    <img src={secondaryLogo} className="mock" alt="Logo-Secondary" />
+    <img src={pictureOne} className="mock" alt="ID w/ Lanyard" />
+    <img src={pictureTwo} className="mock" alt="Letterhead" />
   </li>
   <h3> <strong className="strong">Posters</strong></h3>
   <li>
-    <img src="src\assets\Icawalo (1).png" className="poster" alt="Sport poster" />
-    <img src="src\assets\Icawalo_Typography (1).png" className="poster" alt="Typography" />
+    <img src={pictureThree} className="poster" alt="Sport poster" />
+    <img src={pictureFour} className="poster" alt="Typography" />
   </li>
   <h3><strong className="strong">Photography</strong></h3>
   <li>
-  <img src="src/assets/IMG_3024@362102928.jpg" className="photo" alt="Photo 1" />
-  <img src="src/assets/IMG_3045.jpg" className="photo" alt="Photo 2" />
-  <img src="src/assets/IMG_3166.jpg" className="photo" alt="Photo 3" />
-  <img src="src\assets\6186024448944095280.jpg" className="photo" alt="Photo 4" />
+  <img src={pictureFive} className="photo" alt="Photo 1" />
+  <img src={pictureSix} className="photo" alt="Photo 2" />
+  <img src={pictureSeven} className="photo" alt="Photo 3" />
+  <img src={pictureEight} className="photo" alt="Photo 4" />
   </li>
  
 </ul>
